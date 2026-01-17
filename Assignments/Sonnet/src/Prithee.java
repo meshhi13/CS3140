0f0b3b0d41e5e7e64b0a88e70a48df883ba5b978
Himesh Ahuja <hhhimesh.131@gmail.com>
2026-01-17 18:35:36 -0500

    Finished sonnet

---
+++ Assignments/Sonnet/src/Prithee.java
@@ -0,0 +1,61 @@
+import java.io.IOException;
+import java.util.*;
+
+public class Prithee {
+    private static final String SonnetVerse = "Two households, both alike in dignity, In fair Verona, where we lay our scene, From ancient grudge break to new mutiny, Where civil blood makes civil hands unclean. From forth the fatal loins of these two foes A pair of star-cross’d lovers take their life; Whose misadventured piteous overthrows Do with their death bury their parents’ strife. The fearful passage of their death-mark’d love, And the continuance of their parents’ rage, Which, but their children’s end, nought could remove, Is now the two hours’ traffic of our stage; The which if you with patient ears attend, What here shall miss, our toil shall strive to mend.";
+    private static final String[] SonnetArray = SonnetVerse.split(" ");
+    private static final String[] CleanedSonnetArray = SonnetVerse.replaceAll(",", "").replaceAll("\\.", ",").toLowerCase().split(" ");
+    private static final Scanner input = new Scanner(System.in);
+    public static void promptEnterKey(){
+        System.out.println("Press \"ENTER\" to continue...");
+        try {
+            int _ = System.in.read(new byte[2]);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void pritheeLoop () {
+        int numCorrect = 0;
+        int numIncorrect = 0;
+        while (numCorrect < 3 && numIncorrect < 3) {
+            if (pritheeGame()) {
+                System.out.println("Correct!");
+                numCorrect++;
+            }
+            else {
+                numIncorrect++;
+                System.out.println("Incorrect...");
+            }
+            System.out.printf("You have %d correct and %d incorrect", numCorrect, numIncorrect);
+            System.out.println();
+            if (numCorrect < 3 && numIncorrect < 3) promptEnterKey();
+        }
+        System.out.println("Game over.");
+    }
+
+    public static boolean pritheeGame() {
+        int randomDigit = new Random().nextInt(CleanedSonnetArray.length);
+        for (int i = 0; i < randomDigit; i++) {
+            if (Character.isUpperCase(SonnetArray[i].toCharArray()[0])) {
+                System.out.println();
+            }
+            System.out.print(SonnetArray[i] + " ");
+        }
+
+        for (int i = 0; i < CleanedSonnetArray[randomDigit].length(); i++) {
+            System.out.print("_");
+        }
+        System.out.println();
+        System.out.println("------------------------------------");
+
+        System.out.print("INPUT YOUR GUESS HERE: ");
+
+        String matchInput = input.next();
+        return matchInput.equalsIgnoreCase(CleanedSonnetArray[randomDigit]);
+    }
+
+    public static void main(String[] args) {
+        pritheeLoop();
+    }
+}
